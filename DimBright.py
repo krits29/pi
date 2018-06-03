02b691ad22f1d972ae7c05a15481f4682dea2878
krits29 <kriti.sharma@live.com>
2018-06-03 12:15:38 -0700

    Rainbow FLashing

--- DimBright.py
+++ DimBright.py
@@ -1,18 +1,30 @@
-import RPi.GPIO as IO
+import RPi.GPIO as GPIO
 import time
-IO.setwarnings(False)
-IO.setmode (IO.BCM)
-IO.setup(8,IO.OUT)           
-p = IO.PWM(8,100)          
-p.start(0)
-while 1:
-
-    for x in range (50):
-
-
-        p.ChangeDutyCycle(x)
-        time.sleep(0.1)
-
-    for x in range (50):
-        p.ChangeDutyCycle(50-x)
-        time.sleep(0.1)
+LedPin = 12
+def setup():
+    global p
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.LOW)
+    p = GPIO.PWM(LedPin, 1000)
+    p.start(0)
+def loop():
+    while True:
+        for dc in range(0, 101, 1):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.01)
+        time.sleep(1)
+        for dc in range(100, -1, -1):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.01)
+        time.sleep(1)
+def destroy():
+    p.stop()
+    GPIO.output(LedPin, GPIO.LOW)
+    GPIO.cleanup()
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
---
+++ RainbowLight.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import random
+pins = {'pin_R':11, 'pin_G':12, 'pin_B':13} # pins is a dict
+def setup():
+    global p_R,p_G,p_B
+    GPIO.setmode(GPIO.BOARD)
+    for i in pins:
+        GPIO.setup(pins[i], GPIO.OUT)
+        GPIO.output(pins[i], GPIO.HIGH)
+    p_R = GPIO.PWM(pins['pin_R'], 2000)
+    p_G = GPIO.PWM(pins['pin_G'], 2000)
+    p_B = GPIO.PWM(pins['pin_B'], 2000)
+    p_R.start(0)
+    p_G.start(0)
+    p_B.start(0)
+def setColor(r_val,g_val,b_val):
+    p_R.ChangeDutyCycle(r_val)
+    p_G.ChangeDutyCycle(g_val)
+    p_B.ChangeDutyCycle(b_val)
+def loop():
+    while True :
+        r=random.randint(0,100)
+        g=random.randint(0,100)
+        b=random.randint(0,100)
+        setColor(r,g,b)
+        time.sleep(0.3)
+def destroy():
+    p_R.stop()
+    p_G.stop()
+    p_B.stop()
+    GPIO.cleanup()
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
