8ae69cd5e52c0562894cd785d062fc88e56a0d3e
krits29 <kriti.sharma@live.com>
2018-06-03 18:14:46 -0700

    led bar graph

---
+++ LightWater.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+ledPins = [11, 12, 13, 15, 16, 18, 22, 3, 5, 24]
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    for pin in ledPins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+def loop():
+    while True:
+        for pin in ledPins:
+            GPIO.output(pin, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.output(pin, GPIO.HIGH)
+        for pin in ledPins[10:0:-1]:
+            GPIO.output(pin, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.output(pin, GPIO.HIGH)
+def destroy():
+    for pin in ledPins:
+        GPIO.output(pin, GPIO.HIGH)
+    GPIO.cleanup()
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
